e028b0c5ef4f3c12c35daf1917927414dc9ac46b
DESKTOP-BKSMON7\huy <quanghuytb111@gmail.com>
2023-11-26 12:32:48 +0700

    no message

--- nextjs-dashboard-main/src/lib/index.ts
+++ nextjs-dashboard-main/src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as withAuth } from './withAuth'
 export { default as redirectIfAuthenticated } from './redirectIfAuthenticated'
 export { serializeCookie } from './cookie'
+export const fetcher = (url: string) => fetch(url).then((res) => res.json())
